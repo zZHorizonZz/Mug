365b6d47ee63739ebf03fd9fce4e8546cbef5ca9
Horizon <danfiala23@gmail.com>
2022-03-01 23:42:38 +0100

    Minor work on expression.

--- src/language/native/global/expression.h
+++ src/language/native/global/expression.h
@@ -1,6 +1,18 @@
 #ifndef EXPRESSION_H
 #define EXPRESSION_H
 
+#include "number.h"
+#include "token.h"
+
+typedef struct expression_s {
+    char type;
+
+    value_expression *value_expression;
+    operator_expression *operator_expression;
+    reference_expression *reference_expression;
+    arithmetic_expression *arithmetic_expression;
+} expression;
+
 typedef struct value_expression_s {
     
 } value_expression;
@@ -12,5 +24,16 @@ typedef struct operator_expression_s {
 typedef struct reference_expression_s {
 
 } reference_expression;
+
+typedef struct arithmetic_expression_s {
+    char type;
+
+    number *left_side;
+    number *right_side;
+} arithmetic_expression;
+
+void evaluate_token_block(expression *expression, token *block) {
+
+}
 
 #endif
