3a4362532e4d8e08217ee0762ad6f3d082879c26
Horizon <60035933+zZHorizonZz@users.noreply.github.com>
2022-03-05 00:11:59 +0100

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Projects/Mug/src
-# Build directory: D:/Projects/Mug/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
